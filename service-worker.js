722fdf64f684175bb9ddf01ef4fce8b5eb11d77e
BartdeJong <bart.dejong375@gmail.com>
2019-06-13 20:27:45 +0200

    Changed manifest icons

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6ad1daab401367174797e2c9358041e.js"
+  "/precache-manifest.d3dbc0f978b93f3bee13bacb0aa10053.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mqttdisplayer"});
